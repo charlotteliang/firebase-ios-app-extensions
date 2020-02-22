960a5f81610430af6c5f2c81aa986a1cf64281ce
Chen Liang <chliang@google.com>
2020-02-21 16:33:22 -0800

    add barebone of the sparky app

---
+++ Sparky/Sparky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sparky
+//
+//  Created by Chen Liang on 2/21/20.
+//  Copyright © 2020 Chen Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Sparky/SparkyOnWatchWatchKitExtension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SparkyOnWatch WatchKit Extension
+//
+//  Created by Chen Liang on 2/21/20.
+//  Copyright © 2020 Chen Liang. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
