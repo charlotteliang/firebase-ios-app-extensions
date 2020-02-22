56bd1de9895ffae622c5bdd88e78472a9b653fff
Chen Liang <chliang@google.com>
2020-02-21 16:45:09 -0800

    add minimum function to enable messaging

--- Sparky/Sparky/AppDelegate.swift
+++ Sparky/Sparky/AppDelegate.swift
@@ -1,10 +1,16 @@
+// Copyright 2020 Google
 //
-//  AppDelegate.swift
-//  Sparky
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Chen Liang on 2/21/20.
-//  Copyright © 2020 Chen Liang. All rights reserved.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import UIKit
 import CoreData
@@ -13,12 +19,25 @@ import FirebaseCore
 import FirebaseMessaging
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
 
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+      if granted {
+        application.registerForRemoteNotifications()
+      }
+    }
+    Messaging.messaging().delegate = self
     return true
+  }
+  
+  // MessagingDelegate
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+    print("token:\n" + fcmToken)
   }
 
   // MARK: UISceneSession Lifecycle
