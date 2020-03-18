ed399c2040399bcbd035914ade5127d8422d6f34
Chen Liang <chliang@google.com>
2020-03-17 18:03:49 -0700

    Add share and today folder for codelab (#5)

---
+++ Sparky_dev/Sparky_share/Sparky/Sparky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sparky
+//
+//  Created by Chen Liang on 3/17/20.
+//  Copyright © 2020 Google, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
